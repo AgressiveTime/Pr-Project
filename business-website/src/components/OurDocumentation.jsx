811bce007e4e87a4a5f4ef7773187f918dbc57f8
AgressiveTime <agressivetime9@gmail.com>
2025-10-30 14:01:26 +0300

    commit_0.1

--- business-website/src/components/OurDocumentation.jsx
+++ business-website/src/components/OurDocumentation.jsx
@@ -6,18 +6,31 @@ import BgImg2 from '../assets/background-images/BgImg2.png'
 
 function OurDocumentation() {
   return (
-    <div className='our_documentation'>
-      <div className='o_d-text'>
-        <div className='o_d-text1'>Our Documentation</div>
-        <div className='o_d-text2'>See what our profile is like and how we work for your<br />
-          business</div>
+    <>
+      <div className='our_documentation'>
+        <div className='o_d-text'>
+          <div className='o_d-text1'>Our Documentation</div>
+          <div className='o_d-text2'>See what our profile is like and how we work for your<br />
+            business</div>
+        </div>
+        <div className='group'>
+          <img className='gp_img' src={GroupImg} alt="" />
+          <img className='bg_img1' src={BgImg1} alt="" />
+          <img className='bg_img2' src={BgImg2} alt="" />
+        </div>
+
       </div>
-      <div className='group'>
-        <img className='gp_img' src={GroupImg} alt="" />
-        <img className='bg_img1' src={BgImg1} alt="" />
-        <img className='bg_img2' src={BgImg2} alt="" />
+      <div className='o_d-content'>
+        <div className='content'>
+          <div className='content1'>
+            
+          </div>
+          <div className='content2'>
+
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
